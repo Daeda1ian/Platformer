f529356024aede111e6797d6ef7c7a22681ef43d
Daedalian <daedalianvs@gmail.com>
2025-03-01 17:04:33 +0300

    first commit

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Hero : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpImpulse;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private LayerCheck _layerCheck;
+    [SerializeField] private float _groundCheckRadius;
+    [SerializeField] private Vector3 _groundCheckPositionDelta;
+
+    private float directionX;
+    private float directionY;
+
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+ 
+    public void SetDirectionX(float direction) {
+        directionX = direction;
+    }
+
+    public void SetDirectionY(float direction) {
+        directionY = direction;
+    }
+
+    public void SaySomething() {
+        Debug.Log("Hello!");
+    }
+
+    private bool IsGrounded() {
+        return _layerCheck.isTouchingLayer;
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(directionX * speed, rb.velocity.y);
+
+        if (directionY > 0) {
+            if(IsGrounded()) {
+                rb.AddForce(Vector2.up * jumpImpulse, ForceMode2D.Impulse);
+            }
+        } else if (rb.velocity.y > 0) {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+
+public class HeroInputReader : MonoBehaviour {
+
+    [SerializeField] private Hero _hero;
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context) {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirectionX(direction);
+    }
+
+    public void OnVerticalMovement(InputAction.CallbackContext context) {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirectionY(direction);
+    }
+
+    public void OnSaySomething(InputAction.CallbackContext context) {
+        if(context.canceled) { 
+            _hero.SaySomething();
+        }
+    }
+
+}
